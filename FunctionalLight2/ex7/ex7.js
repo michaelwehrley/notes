57db0dee9be0519ac5d39418c3c5e474a824a326
Michael Wehrley <michaelwehrley@gmail.com>
2019-06-12 13:17:41 -0500

    work with reducing - not filtering

--- FunctionalLight2/ex7/ex7.js
+++ FunctionalLight2/ex7/ex7.js
@@ -81,3 +81,45 @@ function addN(car, ...cdr) {
 }
 
 addN(three, four, five);
+
+
+// 6. Start with an array of odd and even numbers (with some duplicates),
+// and trim it down to only have unique values.
+
+// Iteration
+function inCollection(value, collection) {
+  return collection.includes(value)
+}
+
+function exclude(array, fn) {
+  var collection = [];
+
+  array.forEach(function(value) {
+    if (!fn(value, collection)) {
+      collection.push(value);
+    }
+  });
+
+  return collection;
+}
+
+exclude([1,3,4,3,6,1,12], inCollection);
+
+function inCollection(value, collection) {
+  return collection.includes(value)
+}
+
+newArray = []
+
+[1,3,4,3,6,1,12].filter((value) => inCollection(value, newArray));
+
+const result = words.filter(word => word.length > 6);
+
+[1,3,4,3,6,1,12].filter(function(value, _index, array) {
+  inCollection(value, array);
+})
+
+
+
+
+
